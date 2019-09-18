0305b9abc9b602197ea1ecf3ce20d3e679034d22
EdwardNg-Tech <55099020+EdwardNg-Tech@users.noreply.github.com>
2019-09-18 11:44:42 -0400

    Assessment # 5 09.18

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function largeFilter (i) {
+/* function largeFilter (i) {
     return i > 100;
 }
 let initialBill = [140, 45,270];
@@ -17,4 +17,25 @@ for (i = 0; i < initialBill.length; i++) {
     }
 }
 let largeBill = finalBill.filter(largeFilter);
-console.log(initialBill, finalBill, largeBill);
+console.log(initialBill, finalBill, largeBill); */
+const rob = {
+    fullName: 'Robert Grande',
+    mass: 78,
+    height: 1.69,
+    bmi: function(){
+        return this.mass / (this.height * this.height);
+    }
+}
+const dejon = {
+    fullName: 'Dejon Kurti',
+    mass: 110,
+    height: 1.95,
+    bmi: function(){
+        return this.mass / (this.height * this.height);
+    }
+}
+if (rob.bmi > dejon.bmi) {
+    console.log(`${rob.fullName} has a BMI of ${rob.bmi()}, which is greater than ${dejon.fullName}'s BMI.`);
+} else {
+    console.log(`${dejon.fullName} has a BMI of ${dejon.bmi()}, which is greater than ${rob.fullName}'s BMI.`);
+}
